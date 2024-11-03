fb2aa52130e24f74ab888a36847dc689a29e191f
monty <velimir.vesselinov@gmail.com>
2024-11-03 07:50:55 -1000

    major fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 import DocumentFunction
 
-expected = "**DocumentFunction.documentfunction**\n\nCreate function documentation\n\nMethods:\n - `DocumentFunction.documentfunction(f::Function; location, maintext, argtext, keytext) in DocumentFunction`\nArguments:\n - `f::Function` : Function to be documented\nKeywords:\n - `argtext` : Dictionary with text for each argument\n - `keytext` : Dictionary with text for each keyword\n - `location` : Boolean to show/hide function location on the disk\n - `maintext` : General function description\n"
+expected = "**DocumentFunction.documentfunction**\n\nCreate function documentation\n\nMethods:\n - `DocumentFunction.documentfunction(f::Function; location, maintext, argtext, keytext)`\nArguments:\n - `f` :: `Function` : Function to be documented\nKeywords:\n - `location` : Boolean to show/hide function location on the disk\n - `maintext` : General function description\n - `argtext` : Dictionary with text for each argument\n - `keytext` : Dictionary with text for each keyword\n"
 
 import Test
 
@@ -16,6 +16,7 @@ output = DocumentFunction.documentfunction(DocumentFunction.documentfunction;
 DocumentFunction.documentfunction(DocumentFunction.documentfunction; location=true)
 noargfunction() = nothing
 @Test.testset "Document" begin
+
 	@Test.test output == expected
 	@Test.test [] == DocumentFunction.getfunctionkeywords(DocumentFunction.getfunctionmethods)
 	@Test.test [] == DocumentFunction.getfunctionarguments(noargfunction)
